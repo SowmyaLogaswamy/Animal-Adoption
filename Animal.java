8bf9997e4f79bad447be3ffc28b6bf7daf3f331f
Esvethlynna Pangelinan and Sowmya Logaswamy <esvethlynna@>
2017-06-26 11:01:01 -0700

    add custom method in the custom class, add for loop to list animals below the max age provided by the user

--- Animal.java
+++ Animal.java
@@ -2,4 +2,8 @@ class Animal {
   public int mAge;
   public String mSpecies;
   public String mName;
+
+ public boolean insidePet(int age) {
+   return(mAge < age);
+ }
 }
--- App.java
+++ App.java
@@ -1,30 +1,38 @@
+import java.io.Console;
+
 public class App {
   public static void main(String[] args) {
+    Console myConsole = System.console();
 
-  Animal puppy = new Animal();
-  puppy.mAge = 2;
-  puppy.mSpecies = "Dog";
-  puppy.mName = "Spot";
+    Animal puppy = new Animal();
+    puppy.mAge = 2;
+    puppy.mSpecies = "Dog";
+    puppy.mName = "Spot";
 
-  Animal kitten = new Animal();
-  kitten.mAge = 7;
-  kitten.mSpecies = "Cat";
-  kitten.mName = "Sam";
+    Animal kitten = new Animal();
+    kitten.mAge = 7;
+    kitten.mSpecies = "Cat";
+    kitten.mName = "Sam";
 
-  Animal snake = new Animal();
-  snake.mAge = 15;
-  snake.mSpecies = "Reptile";
-  snake.mName = "Slither";
+    Animal snake = new Animal();
+    snake.mAge = 15;
+    snake.mSpecies = "Reptile";
+    snake.mName = "Slither";
 
-  Animal[] allAnimals = {puppy, kitten, snake};
+    Animal[] allAnimals = {puppy, kitten, snake};
+    System.out.println("What is the maximum age you would prefer for adoption");
+    String stringMaxAge = myConsole.readLine();
+    int maxAge = Integer.parseInt(stringMaxAge);
 
-  System.out.println("All Animals:");
+    System.out.println("Here is the list of animals you can adopt.");
 
-  for(Animal individualAnimal: allAnimals) {
-    System.out.println("----------------");
-    System.out.println(individualAnimal.mAge);
-    System.out.println(individualAnimal.mSpecies);
-    System.out.println(individualAnimal.mName);
+    for(Animal individualAnimal: allAnimals) {
+      if (individualAnimal.insidePet(maxAge)) {
+        System.out.println("----------------");
+        System.out.println(individualAnimal.mAge);
+        System.out.println(individualAnimal.mSpecies);
+        System.out.println(individualAnimal.mName);
+      }
+    }
   }
- }
 }
