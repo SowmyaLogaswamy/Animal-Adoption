424ca804bdf80ef5c8721199e14d41b384f34ab7
Esvethlynna Pangelinan and Sowmya Logaswamy <sowmya@>
2017-06-26 11:29:49 -0700

    build a constructor for the Animal class and instantiated the object.

--- Animal.java
+++ Animal.java
@@ -3,6 +3,12 @@ class Animal {
   public String mSpecies;
   public String mName;
 
+  public Animal(int age, String species, String name) {
+    mAge = age;
+    mSpecies = species;
+    mName = name;
+  }
+
  public boolean insidePet(int age) {
    return(mAge < age);
  }
--- App.java
+++ App.java
@@ -4,20 +4,9 @@ public class App {
   public static void main(String[] args) {
     Console myConsole = System.console();
 
-    Animal puppy = new Animal();
-    puppy.mAge = 2;
-    puppy.mSpecies = "Dog";
-    puppy.mName = "Spot";
-
-    Animal kitten = new Animal();
-    kitten.mAge = 7;
-    kitten.mSpecies = "Cat";
-    kitten.mName = "Sam";
-
-    Animal snake = new Animal();
-    snake.mAge = 15;
-    snake.mSpecies = "Reptile";
-    snake.mName = "Slither";
+    Animal puppy = new Animal(2, "Dog", "Spot");
+    Animal kitten = new Animal(7, "Cat", "Sam");
+    Animal snake = new Animal(15, "Reptile", "Slither");
 
     Animal[] allAnimals = {puppy, kitten, snake};
     System.out.println("What is the maximum age you would prefer for adoption");
